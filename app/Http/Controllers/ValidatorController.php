a764762bef359df334b1fc9ed357d6f194740ad5
Walter Nascimento <walternascimentobarroso@gmail.com>
2022-06-30 13:40:25 +0100

    [ADD] Second validation]
    Signed-off-by: Walter Nascimento <walternascimentobarroso@gmail.com>

--- app/Http/Controllers/ValidatorController.php
+++ app/Http/Controllers/ValidatorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Email;
 
 class ValidatorController extends Controller
 {
@@ -26,13 +27,27 @@ class ValidatorController extends Controller
     {
         $email = $request->input('email');
 
-        $msg = "";
+        $msg = "Success";
+        $valid = true;
 
         // 1º validation - email is empty
         if (!isset($email)) {
             $msg = "Please, fill the input";
+            $valid = false;
         }
 
+        // 2º validation - email already exist
+        $exist = Email::where('email', $email)->first();
+        if (isset($exist)) {
+            $msg = "email already exist";
+            $valid = false;
+        }
+
+        Email::create([
+            'email' => $email,
+            'description' => $msg,
+            'valid' => $valid
+        ]);
 
         return redirect()->route('validator.index')
             ->with('info', $msg);
